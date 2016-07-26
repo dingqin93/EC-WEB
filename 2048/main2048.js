dce80fdc6c4e0f189abaa33b8e418ed36e932044
dingqin93 <dingqin93@sina.com>
2016-07-26 23:06:05 +0800

    Signed-off-by: dingqin93 <dingqin93@sina.com>网页游戏2048

---
+++ 2048/main2048.js
@@ -0,0 +1,50 @@
+var board = new Array();
+var score = 0;
+
+$(document).ready(function(){
+	newgame();
+})
+
+function newgame(){
+	//初始化棋盘格
+	init();
+	//在随机两个格子生成数字
+}
+function init()
+{
+	for (var i = 0; i < 4; i++) 
+		for(var  j = 0; j < 4; j ++)
+		{
+			var gridCell = $('#grid-cell-'+i+'-'+j);
+			gridCell.css('top',getPosTop(i,j));
+			gridCell.css('left',getPosLeft(i,j));
+		}
+	
+	for (var i=0;i<4;i++)
+	{
+		board[i] = new Array();
+		for(var j=0;j<4;j++)
+			board[i][j] = 0;
+	}
+	updateBoardView();
+}
+function updateBoardView(){
+	$(".number-cell")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
